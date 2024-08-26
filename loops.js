88062fadeb5c196bb7c00f8ff51668cef5d5d666
UTKARSH <utkarshganoo@gmail.com>
2024-08-26 16:17:04 +0530

    imp msg to undersatnd for loop

--- loops.js
+++ loops.js
@@ -9,16 +9,16 @@
 //    else {
 //     console.log("Your age is not between 10 and 20");
 //   }
-  //console.log("you are in ",(age<20) ? "man": "older");
+//console.log("you are in ",(age<20) ? "man": "older");
 
-  /*const prompt = require('prompt-sync')();
-  let a = prompt("enter your number");
+/*const prompt = require('prompt-sync')();
+let a = prompt("enter your number");
 a= Number.parseInt(a);
 if (a%2==0 || a%3==0){
-  console.log("your no is divisible by 2")
+console.log("your no is divisible by 2")
 }
 else{
-  console.log("your no id divisible by 2")
+console.log("your no id divisible by 2")
 }*
 
 // to print table
@@ -28,9 +28,12 @@ let n;
 n = prompt("enter the value of n");
 n=Number.parseInt(n);
 for(let i=1; i<=10; i++){
-  sum =n*i;
-  
-  console.log((i),"*",(n),"=",sum);
+      |_ _ _ _ _ _ _  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _> ager ye i ko nichhe wale consolelog 
+                                                             ke niche likhenge to yaha ke i ko let ki jagah var karna padega qki let kewal 
+                                                                       for ke condition bolck ke liye hi chalega per ager var hota toh woh pure code ke 
+                                                                       liye hota
+sum =n*i; 
+ console.log((i),"*",(n),"=",sum);
 }
 // console.log("i " +n+ "n" ,sum);
 */
@@ -49,16 +52,18 @@ for(let bhagwan in obj){
   console.log("this is " + bhagwan + "wife of" +obj[bhagwan]);
 }*/
 
-/*let b;
-let obj = {
-  seeta:'60',
+let b;
+let name = {
+  seeta: '60',
   radha: '40',
   parvati: '50',
-  laxmi : '70',
+  laxmi: '70',
 
 }
-for( b in obj){
- console.log(b + ":" +obj[b]);
-}*/
+for (b in name) {
+  console.log(b + ":" + name[b]);
+}
+
+// for of loop
 
 
