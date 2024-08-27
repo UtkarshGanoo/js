c8a7cd74db4546f223b521f03ce623518723c150
UTKARSH <utkarshganoo@gmail.com>
2024-08-27 17:09:50 +0530

    escape sequence and templet literal string

--- string.js
+++ string.js
@@ -10,4 +10,18 @@ console.log(name[5]);
 console.log(name[6]);
 */
 
- 
+//templet literals
+/*
+let b1='ram';
+let b2='laxman';
+let b3='sita';
+
+let relation = ` ${b1} " is a brother of " ${b2} " and  husband of " ${b3} `;
+console.log(relation);
+ */
+
+
+// escape sequence charecter
+/*
+let name = 'utkar\'sh'
+console.log(name);*/
