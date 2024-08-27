dc745ed4c6f3bfda05b1d7adb74cf103adfde80b
UTKARSH <utkarshganoo@gmail.com>
2024-08-27 15:16:10 +0530

    in 'u t k a ' why to printthis

---
+++ string.js
@@ -0,0 +1,13 @@
+/* 
+let name ="utkarsh";
+console.log(name);
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+console.log(name[3]);
+console.log(name[4]);
+console.log(name[5]);
+console.log(name[6]);
+*/
+
+ 
