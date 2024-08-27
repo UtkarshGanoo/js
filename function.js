c9330d09cacc76df65cb933a0263ba756f04aad4
UTKARSH <utkarshganoo@gmail.com>
2024-08-27 14:46:33 +0530

    practice of function and loops

--- function.js
+++ function.js
@@ -1,8 +1,48 @@
 //first function program
-function addnum(a,b){
+/*function addnum(a,b){
     return a+b;
 }
 
 let a = 10;
 let b = 20;
-console.log("the sum fo no is",addnum(a,b));
+console.log("the sum fo no is",addnum(a,b));*/
+
+
+
+
+// loops and function practice
+
+//  que 1
+
+/*let obj = {
+    harry :'80',
+    rohan : '70',
+    aakash :'60',
+}
+for(let b in obj ){
+    console.log("marks of" + b + "is" +obj[b]  );
+}*/
+
+
+// que 2 
+/*
+const prompt=require('prompt-sync')();
+let n;
+let i=10;
+n=Number.parseInt(n);
+while(n!=i){
+    console.log("try again !")
+n=prompt("Enter The Vlaue Of n"); 
+}
+console.log("you have enter correct no. of n );
+*/
+
+
+// que 3
+
+/*
+const mean = (a,b,c,d,e)=>{
+
+    return (a+b+c+d+e)/5;
+}
+console.log(mean(5,6,7,8));*/
