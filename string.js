dabde102e9a3d1a8afbbfea1d4e612dde45be759
UTKARSH <utkarshganoo@gmail.com>
2024-08-28 00:56:17 +0530

    practice nad understand string

--- string.js
+++ string.js
@@ -25,3 +25,82 @@ console.log(relation);
 /*
 let name = 'utkar\'sh'
 console.log(name);*/
+
+//string methods practice
+
+
+//slice() function method
+/*
+let god = 'krishna ';
+console.log(god.slice(0,5));*/
+
+//length 
+/*
+let emp = "darshan";
+console.log(emp.length);*/
+
+
+
+//to uppercase ()
+/*
+
+let fruit = 'apple';
+console.log(fruit.toUpperCase());
+*/
+
+
+
+//toLowercse
+/*
+
+let Men = 'ROHAN';
+console.log(Men.toLowerCase());
+*/
+
+
+// concate()
+/*
+let god = 'ram';
+let god2 ='laxman';
+console.log(god.concat(" is a brother of ",  god2));
+*/
+
+// replace()
+/*
+let fruit = 'apple is banana';
+
+console.log(fruit.replace("banana","redwine"));
+*/
+
+// trim()
+/*
+let my  = '     my name is utkarsh    ';
+
+console.log(my.trim());
+*/
+
+// char at index 
+/*
+let sentence = 'the india is largest countryin the world';
+console.log(`at the index of  ${(sentence.charAt(5))}`)
+*/
+
+
+// practice of the string 
+
+// que 1
+// console.log('har\"'.length);
+
+
+//que 3
+
+/*let a = 'RAM'
+console.log(a.toLowerCase());*/
+
+// que 4
+/*
+let str = 'pls give me rs 2000';
+let str2 = Number.parseInt(str.slice("pls give me rs ".length));
+console.log(str2);
+console.log(typeof str2);*/
+
