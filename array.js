fb3a54ff856a3a4b7d025111674fbf44724366e0
UTKARSH <utkarshganoo@gmail.com>
2024-08-29 18:12:08 +0530

    all loop and array concept wiith example

---
+++ array.js
@@ -0,0 +1,122 @@
+// to print the array iwth using for loop
+/*
+let arr=[20,400,50,80,"not",true];
+
+for(let i=0; i<arr.length; i++){
+    console.log(arr[i]);
+}*/
+
+
+
+/*
+let a =[10,20,30,40,50,60,"utkarsh",true];
+console.log(a); // to print the element of array
+console.log(a[0]);// to print the array with the help of index value 
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+// a [8] = 'ganoo';
+// console.log(a)
+a [7] = 'ganoo';// this two for replace the value of the array
+console.log(a)
+a [8] = true;
+console.log(a);*/
+
+
+
+//patctice of array methods
+
+// let num = [5,6,5,8,5];
+// let b= num.toString();
+// console.log(b);
+
+// let hum = num.join('bane');
+// console.log(hum);
+
+//let  r = num.pop();
+// num.pop();
+//console.log(num,r)
+
+// let  r = num.push(99);
+// num.pop();
+// console.log(num,r)
+
+// let  r = num.shift();
+// console.log(r,num)
+
+// let  r = num.unshift(6);
+// console.log(r,num)
+
+/*
+let num = [1,2,3,4,5,6,7,8,9];
+let hum = [15,20,25,30,35,40,45];
+let mum = [454,265,754,788,456,685];
+
+let my_arr = num.concat(hum,mum);
+console.log(my_arr);*/
+
+// sort method()
+/*
+let arr = [4,2,5,6,3,5,8]
+arr.sort()
+console.log(arr)
+*/
+
+// slice and splice 
+/*{
+let ary=[2,5,4,56,5,6,8,56,6,5]
+let b=ary.slice(3)
+console.log(b)}
+
+let ary=[2,5,4,56,5,6,8,56,6,5]
+ary.splice(3,3,4,5,6)
+console.log(ary)*/
+
+// loops using in array 
+/*let arr =[1,2,3,4,5,6,7];
+// let arr1 =[9,10,11,12];
+//   let arr2= arr.concat(arr1);
+// for (let i= 0; i<arr2.length; i++ ){
+for (let i= 0; i<arr.length; i++ ){
+    console.log(arr[i]);
+}*/
+// which loop use for aray
+/*let my_arr = [1,2,3,4,6,5,6,8]
+let i=0
+while(i<my_arr.length ){
+    console.log(arr[i]);
+    i++;
+}*/
+
+
+// for each loop 
+
+// let num = [1,2,5,6,8,3,5,2,6,6,2,5]
+// num.forEach((element)=>{
+    // console.log(element*element);
+// });
+ 
+// for (let mum of num){
+//     console.log(mum);
+// }
+
+// for (let mum in num){
+//     console.log(mum);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
