853eb3a0edd6394c0d88959377ca7feccd1a5803
UTKARSH <utkarshganoo@gmail.com>
2024-08-30 11:41:55 +0530

    map filter reduce

---
+++ mapfit.js
@@ -0,0 +1,27 @@
+// filter method 
+/*let myarr =[ 4,45,8,2,4,82,4,45,54,54,5,5];
+let a = myarr.filter((a)=>{
+    return a<10;
+}) 
+co nsole.log(a);*/
+
+
+//map method
+
+/*
+let mark = [7,8,5,54,6,12,4]
+
+ let c= mark.map((value,index)=>{
+    return value +2 ;
+    // console.log(value,index);
+}) 
+console.log(c);*/
+
+// REDUCE METHOD
+
+/*
+let utk = [5,3,5,4,6,5];
+let gan = utk.reduce((a,b)=>{
+    return a+b;
+})
+console.log(gan)*/
