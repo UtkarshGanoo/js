7b39a12d0de75fa8e2774ab114545c8858f8a874
UTKARSH <utkarshganoo@gmail.com>
2024-08-30 13:47:29 +0530

    practice of array

---
+++ practic array.js
@@ -0,0 +1,41 @@
+    /*
+    let prompt=require('prompt-sync')();
+    let arr =[1,2,3,4,5,6]
+    let a=prompt("Enter The Number")
+    a=Number.parseInt(a)
+    arr.push(a)
+    console.log(arr)*/
+
+
+   /* let prompt=require('prompt-sync')();
+    let arr =[1,2,3,4,5,6]
+    let a;
+    do {
+        
+        a=prompt("Enter The Number");
+        a=Number.parseInt(a);
+        arr.push(a);
+    }while(a!=0);
+    console.log(arr);*/
+
+/*
+    let my_arr=[140,77,50,4,0,56,132,10,210]
+    let a=my_arr.filter((b)=>{
+        return  b%10==0;
+    })
+    console.log(a);*/
+
+/*
+    let num=[2,5,3,6,7,4];
+   let b= num.map((x)=>{
+        return x*x;
+
+    }) 
+    console.log (b);*/
+
+/*
+    let fact=[4,5,0,24,24,156,12,21]
+    let factorial=fact.reduce((f1,f2)=>{
+        return f1*f2;
+    })
+    console.log(factorial)*/
