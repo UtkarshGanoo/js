7166d74aa14ad1a971421d47312ad4223bb6087a
UTKARSH <utkarshganoo@gmail.com>
2024-09-02 20:09:43 +0530

    new file

---
+++ myhtmlcssjs.js
@@ -0,0 +1,5 @@
+document.body.style.backgroundColor = 'Aqua'
+let arr = Array.from(documet.body.firstchild)
+a=document.firstChild
+console.log(a.documentParentNode)
+console.log(a.documentParenteet)
