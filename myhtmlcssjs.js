b35e6f3f5aeaea4b0ddb78dcdbe1545eac9aa3d1
UTKARSH <utkarshganoo@gmail.com>
2024-09-05 21:43:44 +0530

    new methods of dom manipulation

--- myhtmlcssjs.js
+++ myhtmlcssjs.js
@@ -1,5 +1,9 @@
 document.body.style.backgroundColor = 'Aqua'
-let arr = Array.from(documet.body.firstchild)
-a=document.firstChild
-console.log(a.documentParentNode)
-console.log(a.documentParenteet)
+// let arr = Array.from(document.body.firstchild)
+// a=document.firstChild
+// console.log(a.documentParentNode)
+// console.log(a.documentParenteet)
+// let first= document.div.setAttribute("class","myid")
+// ($0.hidden) =false
+let a = document.firstChild.setAttribute('ganoo')
+console.log(utkarsh.dataset)
