fee51e047078838fc82463c4104f1c8546840afa
UTKARSH <utkarshganoo@gmail.com>
2024-09-08 15:02:46 +0530

    settimeout

--- myhtmlcssjs.js
+++ myhtmlcssjs.js
@@ -1,6 +1,4 @@
 document.body.style.backgroundColor = 'Aqua'
-let a = document.getElementsByTagName("div")[0]
-// a.innerHTML= a.innerHTML +'<h1> hello utkarsh</h1> '
-let  div = document.createElement('div');
-div.innerHTML = '<h1>HELLO WORLD</h1>';
-a.appendchild(div)
+setTimeout(() => {
+    alert("my name is utkarsh ganoo")
+}, 2000);
