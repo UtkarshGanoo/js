eee3e9d592aba823c025051e7aadc9de819035d8
UTKARSH <utkarshganoo@gmail.com>
2024-09-08 15:38:31 +0530

    setinterval function with argument

--- myhtmlcssjs.js
+++ myhtmlcssjs.js
@@ -3,7 +3,7 @@ document.body.style.backgroundColor = 'Aqua'
 // const a = 5
 // const b = 5
 // let c = a + b
-setInterval(function(a,b){
+// setInterval(function(a,b){
     
-    alert("my name is utkarsh ganoo"+ (5+5))
-}, 5000);
+//     alert("my name is utkarsh ganoo"+ (5+5))
+// }, 5000);
