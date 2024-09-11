da0db65b8f6e068fe754f80d1ffd04e57af7aca4
UTKARSH <utkarshganoo@gmail.com>
2024-09-11 16:59:47 +0530

    callback and some more topic cover

---
+++ Browser event.js
@@ -0,0 +1,11 @@
+let btn=document.body.getElementsByClassName('btn')[0]
+// let prompt=require('prompt-sync')();
+let x=function(e){ 
+
+    // alert('you enter lessthen 2 no')
+}
+btn.addEventListner('click',x)
+if(a<2){
+    btn.removeEventListner('click',x)
+    alert('you enter lessthen 2 no')
+}
