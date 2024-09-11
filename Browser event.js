1b53fc533e363a5765c84bbfdb9eccc4291138f8
UTKARSH <utkarshganoo@gmail.com>
2024-09-11 17:12:30 +0530

    formate doc

--- Browser event.js
+++ Browser event.js
@@ -1,11 +1,3 @@
-let btn=document.body.getElementsByClassName('btn')[0]
-// let prompt=require('prompt-sync')();
-let x=function(e){ 
-
-    // alert('you enter lessthen 2 no')
-}
-btn.addEventListner('click',x)
-if(a<2){
-    btn.removeEventListner('click',x)
-    alert('you enter lessthen 2 no')
-}
+document.getElementsById("google").addEventListener('click',function(){
+    alert("i am opening google")
+})
---
+++ callback.js
@@ -0,0 +1,58 @@
+// setTimeout(function(){
+//     console.log('hey i am utkarsh');
+    
+// }, 11000);
+
+//callback
+//synchronous callback program
+/*function mul(a,b){
+    console.log(a*b);
+}
+
+
+function answer(a,b,mul){
+    mul(a,b);
+}
+
+answer(2,2,mul)*/
+
+
+//asynchronous callback programing
+/*function sum(a,b){
+c=a+b
+    console.log(c)
+
+}
+
+setTimeout(sum,3000,47,5)*/
+
+
+
+//callback hell problem
+
+function getdata(dataid,getnxtdata){
+    setTimeout(()=>{
+        console.log("data",dataid);
+        if(getnxtdata){
+            getnxtdata();
+        }
+    },3000);
+}
+
+console.log("your data is generating.........!")    //-------------------------------------//|
+getdata(1,()=>{                                                                            //|
+    console.log("your data is getting 1............!")                                     //|
+    getdata(2,()=>{                                                                        //|
+        console.log("your data is getting 2............!")                                 //|
+        getdata(3,()=>{                                                                    //|----------------------------callback hell problem (pyramid)
+            console.log("your data is getting 3............!")                             //|
+            getdata(4,()=>{                                                                //|
+                console.log("your data is getting 4............!")                         //|
+                getdata(5,()=>{                                                            //|
+                    console.log("your data is getting 5............!")//-------------------//|
+
+                });
+            });
+        });
+    });
+});
