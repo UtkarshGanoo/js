17a974457d9625bf8e7639ede7fc222cc6779c27
UTKARSH <utkarshganoo@gmail.com>
2024-09-15 01:40:46 +0530

    all about error handaling

---
+++ try&catch.js
@@ -0,0 +1,43 @@
+/*console.log("utkrash we are in error handlling")
+try{
+
+    console.log(utkarsh)
+}
+catch(error){
+    console.log("utkarsh ko define nahi kiye ho bhai.......!")
+}*/
+
+
+// error object and custom error generate
+/*console.log("utkrash we are in error handlling")
+try{
+
+    console.log(utkarsh)
+}
+catch(error){
+
+    
+    console.log(error.name)
+    console.log(error.message)
+    // console.log("utkarsh ko define nahi kiye ho bhai.......!")
+}*/
+//throw new error
+
+/*try {
+    throw new ReferenceError("utkarsh is good");
+} 
+catch (error) {
+    console.log(error.name)
+    console.log(error.message)
+}*/
+/*try{
+    let age =prompt("enter the age ")
+    if (age>100){
+       throw new ReferenceError("age shise dal le mjak mat kar")
+    }
+}
+catch(error){ 
+    console.log(error.name)
+    console.log(error.message)
+    
+}*/
