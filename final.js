1d4b3810760c3dda2181890925200c557c32bcef
UTKARSH <utkarshganoo@gmail.com>
2024-09-15 15:35:32 +0530

    finally block completed

---
+++ final.js
@@ -0,0 +1,10 @@
+try {
+    console.log("hii utkarshyou are in finally block")
+    console.log(pro)
+}
+catch (error) {
+    console.log(error)
+}
+finally {
+    console.log("dont worry kuch bhi ho jaye mai toh chalunga hi tum chinta mat karo chae error q na aa jaye")
+}
