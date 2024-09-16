8d4255c231c995cb9747865d830b0c4118e33488
UTKARSH <utkarshganoo@gmail.com>
2024-09-16 16:18:51 +0530

    all about fetch api

---
+++ fetchapi.js
@@ -0,0 +1,11 @@
+//fetch weather of the city in console
+/*let p= fetch("https://goweather.herokuapp.com/weather/ny")
+p.then((Response)=>{
+return Response.json()
+})
+.then((value2)=>{
+    console.log(value2)
+})*/
+
+
+//
