45099ed49d8ca650377f7212e67ef130bcaad60f
UTKARSH <utkarshganoo@gmail.com>
2024-09-21 18:30:58 +0530

    local & session storage

--- fetchapi.js
+++ fetchapi.js
@@ -19,3 +19,52 @@ console.log(document.cookie)*/
 let value = prompt("enter the vale")
 document.cookie= `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
 document.cookie= `${(key)}${(value)}`*/
+
+
+//local storage 
+/*
+let key = prompt(" bol bhidu kya key set karega");
+let value  = prompt(" bol bhidu kya value set karega ");
+localStorage.setItem(key,value);
+localStorage.getItem(key);
+if(key=="poorva" || key=="meena"){
+localStorage.removeItem(key);
+}
+
+
+
+if(key==0){
+    localStorage.clear(key);
+}
+localStorage.length*/
+
+//sesson storage
+/*
+let key = prompt(" bol bhidu kya key set karega");
+let value  = prompt(" bol bhidu kya value set karega ");
+sessionStorage.setItem(key,value);
+sessionStorage.getItem(key);
+if(key=="poorva" || key=="meena"){
+sessionStorage.removeItem(key);
+}
+
+
+
+if(key==0){
+    sessionStorage.clear(key);
+}
+sessionStorage.length/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
