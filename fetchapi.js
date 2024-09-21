166c7b520a26ee8dcb25e76bd58dca59c8fd3df9
UTKARSH <utkarshganoo@gmail.com>
2024-09-21 17:50:16 +0530

    encode decode and cookie

--- fetchapi.js
+++ fetchapi.js
@@ -6,3 +6,16 @@ return Response.json()
 .then((value2)=>{
     console.log(value2)
 })*/
+
+//cookie
+/*console.log(document.cookie)
+document.cookie="name == utkarsh "
+console.log(document.cookie)*/
+
+//cookie encode and decode 
+
+
+/*let key = prompt("enter the key")
+let value = prompt("enter the vale")
+document.cookie= `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+document.cookie= `${(key)}${(value)}`*/
