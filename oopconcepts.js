471f868615b584821413719806a40aa6552041b4
UTKARSH <utkarshganoo@gmail.com>
2024-09-24 23:10:42 +0530

    constructer

--- oopconcepts.js
+++ oopconcepts.js
@@ -36,3 +36,27 @@ let sub = new Bus()
  
 utkarsh.ticket()
 sub.submit()*/
+
+
+//constructor
+/*
+class Bus{
+    constructor(){
+        console.log("constructor called")
+    }
+    ticket (){
+       alert("ticket toh le pehle bhai")
+    }
+    submit(){
+        alert("abe sale ticket le ke toh ja seat chahiye toh")
+    }
+}
+   
+let utkarsh = new Bus()
+let sub = new Bus()
+ 
+utkarsh.ticket()
+sub.submit()*/
+
+
+//inheritance & extands keyword
